603f60c71a8009e19f31303cb612b2fd141ec783
WilsonReese <reese@chicagobooth.edu>
2021-04-23 00:19:20 +0000

    set-feed-as-root-and-updated-it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  
   def show
     @user = User.find_by!(username: params.fetch(:username))
   end
 
   def feed
-    @user = User.find_by!(username: params.fetch(:username))
+    # @user = User.find_by!(username: params.fetch(:username))
   end
 
   def followers
